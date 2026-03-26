e4449696bbab2af51a43623975c9ee4e40363d9d
Yadnesh Bhaisare <ybhaisare31@gmail.com>
2026-03-26 18:35:09 +0530

    Updated VPC module/variables.tf

--- vpc-module/variables.tf
+++ vpc-module/variables.tf
@@ -1,11 +1,11 @@
 #VARIABLES
 
 variable "vpc_cidr" {
-    description = "CIDR block for VPC"
-    type = string
+  description = "CIDR block for VPC"
+  type        = string
 }
 
 variable "vpc_name" {
-    description = "name of vpc"
-    type = string
+  description = "Name of VPC"
+  type        = string
 }
